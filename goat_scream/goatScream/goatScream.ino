be34a01a6d96fa65a72156230230188c79e8c64a
JacobDPoland <45803275+JacobDPoland@users.noreply.github.com>
2024-05-25 17:19:28 -0500

    Added cmd and audio player to automatically close

--- goat_scream/goatScream/goatScream.ino
+++ goat_scream/goatScream/goatScream.ino
@@ -1,28 +1,13 @@
 #include <Keyboard.h>
 
 void setup() {
-  Keyboard.begin();
-  delay(500);
-  Keyboard.press(KEY_LEFT_GUI);
-  Keyboard.releaseAll();
-  delay(500);
 
-  Keyboard.print("cmd");
-  delay(500);
-  Keyboard.write(KEY_RETURN);
-  delay(500);
-  
-  Keyboard.println("curl -L -o ScreamingGoat.mp3 https://github.com/JacobDPoland/MyArduinoScripts/raw/master/goat_scream/GoatScream.mp3");
-  delay(1000 * 3);
-
-  Keyboard.println("start ScreamingGoat.mp3");
-  delay(1000 * 3);
-
-  Keyboard.end();
 }
 
 void loop() {
-  delay(1000 * 60);
+  for(int i = 0; i < 2; i++){
+    delay(30000);
+  }
 
   Keyboard.begin();
   delay(500);
@@ -38,8 +23,12 @@ void loop() {
   Keyboard.println("curl -L -o ScreamingGoat.mp3 https://github.com/JacobDPoland/MyArduinoScripts/raw/master/goat_scream/GoatScream.mp3");
   delay(1000 * 3);
 
-  Keyboard.println("start ScreamingGoat.mp3");
-  delay(1000 * 3);
+  Keyboard.println("start ScreamingGoat.mp3\nexit");
+  delay(1000 * 7);
+
+  Keyboard.press(KEY_LEFT_ALT);
+  Keyboard.press(KEY_F4);
+  Keyboard.releaseAll();
 
   Keyboard.end();
 }
